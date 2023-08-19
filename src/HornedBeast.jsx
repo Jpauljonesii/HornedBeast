aa11c83b0c7914281348ae0d1df7a69f1483bd5e
John P. Jones II <jpauljonesii@gmail.com>
2023-08-19 17:43:26 -0500

    add like functionality

--- src/HornedBeast.jsx
+++ src/HornedBeast.jsx
@@ -2,16 +2,36 @@ import React, { Component } from 'react'
 import Gallery from './Gallery'
 
 export default class HornedBeast extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      numberOfFavorites: 0,
+    };
+  }
+  handleClick = ()=>{
+this.setState({numberOfFavorites:this.state.numberOfFavorites + 1 })
+  }
+
   render() {
+    const hornedBeastImages = this.props.image_url;
     return (
       <>
         <div>
           <h2>{this.props.title}</h2>
-          <img src={this.props.imageUrl}></img>
+          <img
+            alt=""
+            onClick={this.handleClick}
+            src={this.props.image_url}
+          ></img>
           <p>{this.props.description}</p>
-          
+           {this.state.numberOfFavorites !==0 &&<>
+           <p>❤️️</p> 
+          <p>Number of Likes: {this.state.numberOfFavorites}</p>
+           </>}
         </div>
       </>
     );
   }
 }
+
+
