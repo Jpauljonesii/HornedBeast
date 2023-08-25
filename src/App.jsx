ed6ac554acfa8af2015dca4f8547a45fea950374
John P. Jones II <jpauljonesii@gmail.com>
2023-08-24 22:04:03 -0500

    set state in App add modal function

--- src/App.jsx
+++ src/App.jsx
@@ -4,15 +4,28 @@ import './App.css'
 import './index.css'
 import 'bootstrap/dist/css/bootstrap.css';
 
+
 import Gallery from './Gallery'
 import Header from './Header'
 import Footer from './Footer'
+import SelectedBeast from './SelectedBeast';
 export default class App extends Component {
+constructor(props){
+  super(props);
+  this.state = {
+    openModal: false, 
+    selectedBeast: {},
+  }
+}
+ handleClose = () => this.setState({openModal: false});
+ handleShow = () => this.setState({openModal: true});
+  
   render() {
   
     
     return (
       <>
+      <SelectedBeast show = {this.state.openModal} handleClose = {this.handleClose}/>
         <div>
       
           <Header></Header>
--- src/SelectedBeast.jsx
+++ src/SelectedBeast.jsx
@@ -1,9 +1,28 @@
 import React, { Component } from 'react'
+import {Modal, Button} from 'react-bootstrap' 
+
 
 export default class SelectedBeast extends Component {
+
   render() {
     return (
-      <div>SelectedBeast</div>
+
+
+      <Modal show={this.props.show} onHide={this.props.handleClose} animation={false}>
+        <Modal.Header closeButton>
+          <Modal.Title>Modal heading</Modal.Title>
+        </Modal.Header>
+        <Modal.Body>Woohoo, you are reading this text in a modal!</Modal.Body>
+        <Modal.Footer>
+          <Button variant="secondary" onClick={this.props.handleClose}>
+            Close
+          </Button>
+          <Button variant="primary" onClick={this.props.handleClose}>
+            Save Changes
+          </Button>
+        </Modal.Footer>
+      </Modal>
+      
     )
   }
 }
